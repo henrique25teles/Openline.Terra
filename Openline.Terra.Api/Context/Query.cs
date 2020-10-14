b35d251ba79e8e17cec2011d60fb2e5d036b88b5
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2020-10-14 00:56:05 -0300

    Refatoração do método query

--- Openline.Terra.Api/Context/Query.cs
+++ Openline.Terra.Api/Context/Query.cs
@@ -19,6 +19,7 @@ namespace Openline.Terra.Api.Context
 
         Dictionary<string, string> _colunas = new Dictionary<string, string>();
         List<Criterio> _criterios = new List<Criterio>();
+        List<Order> _ordenacao = new List<Order>();
         int? _limit;
         int? _offset;
 
@@ -63,6 +64,45 @@ namespace Openline.Terra.Api.Context
             });
         }
 
+        public void OrderBy(Expression<Func<T, object>> property, OrderDirection orderDirection)
+        {
+            var expressao = property.Body.ToString();
+            var indexInicial = expressao.IndexOf('.');
+            var indexFinal = expressao.IndexOf(',');
+
+            var campo = expressao.Substring(indexInicial + 1, indexFinal - indexInicial - 1);
+
+            var coluna = _colunas[campo];
+
+            _ordenacao.Add(new Order
+            {
+                Coluna = coluna,
+                OrderDirection = orderDirection,
+            });
+        }
+
+        public void OrderBy(string campo, OrderDirection orderDirection)
+        {
+            string coluna = "";
+
+            bool isPossuiChave = _colunas.TryGetValue(campo, out coluna);
+
+            if (isPossuiChave)
+            {
+                coluna = _colunas[campo];
+            }
+            else
+            {
+                coluna = campo;
+            }
+
+            _ordenacao.Add(new Order
+            {
+                Coluna = coluna,
+                OrderDirection = orderDirection,
+            });
+        }
+
         public void Take(int limit)
         {
             _limit = limit;
@@ -81,6 +121,7 @@ namespace Openline.Terra.Api.Context
             var sql = $"SELECT {colunas} FROM {nomeTabela} ";
 
             sql = sql + this.GeraWhere();
+            sql = sql + this.GeraOrderBy();
 
             if (_limit.HasValue) sql = sql + $"LIMIT {_limit.Value} ";
             if (_offset.HasValue) sql = sql + $"OFFSET {_offset.Value} ";
@@ -133,13 +174,13 @@ namespace Openline.Terra.Api.Context
 
             if (this._criterios.Any())
             {
-                sql = sql + "where ";
+                sql = sql + "WHERE ";
 
                 foreach (var criterio in this._criterios)
                 {
                     if (this._criterios.IndexOf(criterio) > 0)
                     {
-                        sql = sql + "and ";
+                        sql = sql + "AND ";
                     }
 
                     sql = sql + criterio.Coluna + " ";
@@ -184,13 +225,51 @@ namespace Openline.Terra.Api.Context
 
             return sql;
         }
+
+        private string GeraOrderBy()
+        {
+            var sql = "";
+
+            if (this._ordenacao.Any())
+            {
+                sql = sql + "ORDER BY ";
+
+                foreach (var order in this._ordenacao)
+                {
+                    if (this._ordenacao.IndexOf(order) > 0)
+                    {
+                        sql = sql + ", ";
+                    }
+
+                    sql = sql + $"{order.Coluna} ";
+
+                    switch (order.OrderDirection)
+                    {
+                        case OrderDirection.Asc:
+                            sql = sql + "ASC ";
+                            break;
+                        case OrderDirection.Desc:
+                            sql = sql + "DESC ";
+                            break;
+                    }
+                }
+            }
+
+            return sql;
+        }
     }
 
-    public class Criterio
+    internal class Criterio
     {
         public string Coluna { get; set; }
         public TipoCriterio TipoCriterio { get; set; }
         public object Valor { get; set; }
+    }
+
+    internal class Order
+    {
+        public string Coluna { get; set; }
+        public OrderDirection OrderDirection { get; set; }
     }
 
     public enum TipoCriterio
@@ -203,5 +282,11 @@ namespace Openline.Terra.Api.Context
         MenorOuIgual,
         Contem,
         NaoContem,
+    }
+
+    public enum OrderDirection
+    {
+        Asc,
+        Desc
     }
 }
